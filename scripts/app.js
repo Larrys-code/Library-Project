b101ef09bad56bcdcfc7e6665f293a31aae21e6b
Larry <38928644+Larrys-code@users.noreply.github.com>
2023-04-19 16:47:35 +0100

    Create basic book functions

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+const myLibrary = [];
+
+/**
+ * Creates a Book object from given variables. Title and author
+ * take strings, pages is just the number of pages, and read is
+ * a boolean.
+ * @param {string} title -The title of the Book
+ * @param {string} author -The name of the author
+ * @param {(string|number)} pages -The number of pages
+ * @param {boolean} [read = false] -Has this book been read
+ */
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  /**
+   * Gives info about the Book object as a string.
+   */
+  this.info = () =>
+    `${this.title} by ${this.author}, ${this.pages}, ${
+      this.read ? "finished reading" : "not read yet"
+    }`;
+}
+
+/**
+ * Takes given Book objects and pushes them into
+ * the given Library array (An array of books).
+ * @param {[]} library -Library array
+ * @param  {...Book} books -One or more Book objects to add to the Library
+ */
+function addBooksToLibrary(library, ...books) {
+  books.forEach((book) => {
+    library.push(book);
+  });
+}
+
+function displayLibrary(library) {
+  const bookShelf = document.querySelector();
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295");
+console.log(theHobbit.info());
+addBooksToLibrary(myLibrary, theHobbit);
